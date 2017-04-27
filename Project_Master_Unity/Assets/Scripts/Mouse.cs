92eb5d80626c5c1fca56b2a76736694fb00a5e58
SteCol <stef.col@hotmail.com>
2017-04-27 19:06:38 +0200

    Split Gyro and Mouse inputs into different scripts.

---
+++ Project_Master_Unity/Assets/Scripts/Mouse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    [Header("For Mouse Input")]
+    public float mouseSpeed;
+    public GameObject gimbleA, gimbleB;
+
+    void Start()
+    {
+        Debug.Log("Starting in Mouse mode.");
+    }
+
+    void Update()
+    {
+            PlayMouse();
+    }
+
+    void PlayMouse()
+    {
+        gimbleA.transform.Rotate(0, Input.GetAxis("Mouse_X") * mouseSpeed, 0);
+        gimbleB.transform.Rotate(-Input.GetAxis("Mouse_Y") * mouseSpeed, 0, 0);
+
+    }
+}
