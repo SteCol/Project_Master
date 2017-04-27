c1f70b83b03078085788b3c9f6baa75b32d292f5
SteCol <stef.col@hotmail.com>
2017-04-27 21:13:56 +0200

    Made objects .fbx files and added Steve Movement.

---
+++ Project_Master_Unity/Assets/Scripts/Steve.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steve : MonoBehaviour {
+
+    [Header("Setup")]
+    public float moveSpeed;
+
+	void Update () {
+        if (Input.GetKey(KeyCode.Z)) {
+            this.transform.Translate(GameObject.FindGameObjectWithTag("MainCamera").transform.forward * moveSpeed * Time.deltaTime);
+        }
+	}
+}
