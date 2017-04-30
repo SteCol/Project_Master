e5b1647829d8740fe49446d2799af692aeba921f
Stef Colruyt <stef.col@hotmail.com>
2017-04-30 17:05:25 +0200

    Added closing application feature.

---
+++ Project_Master_Unity/Assets/Scripts/ApplicationControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationControl : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Close_Application"))
+        {
+            StartCoroutine(GameObject.FindGameObjectWithTag("GameController").GetComponent<Debugger>().iDeb("Closing Application", 100));
+
+            Application.Quit();
+        }
+    }
+}
