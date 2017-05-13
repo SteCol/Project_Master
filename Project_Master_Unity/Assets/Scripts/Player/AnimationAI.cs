ef78f19a2fffde424b981f12bae78e8b1d2236a2
Stef Colruyt <stef.col@hotmail.com>
2017-05-13 15:55:30 +0200

    Made FXB's from Hungry models, reorganised some folders, split up animations, set up animation controller, wrote AnimatorAi script, implemented model into level for testing. (Way too much for one commit)

---
+++ Project_Master_Unity/Assets/Scripts/Player/AnimationAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimationAI : MonoBehaviour
+{
+
+    [Header("Setup")]
+    public Animator animator;
+    public NavMeshAgent nma;
+    public Rigidbody rb;
+
+    [Header("Controls")]
+    public bool sitting;
+    public bool moving;
+    public float speed;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        nma = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = nma.velocity.magnitude;
+        Debug.Log("rb velocity" + speed);
+        animator.SetFloat("Speed", speed);
+
+        if (speed > 0.0f)
+            animator.SetBool("Moving", true);
+        else
+            animator.SetBool("Moving", false);
+
+        animator.SetBool("Sitting", sitting);
+    }
+}
