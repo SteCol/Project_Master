a3c01b9cc4fb989dee7ee011a96753fe29355895
Stef Colruyt <stef.col@hotmail.com>
2017-05-14 01:26:03 +0200

    Loads of fucking about.

---
+++ Project_Master_Unity/Assets/Scripts/CustomGoogleVR.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Google;
+
+public class CustomGoogleVR : MonoBehaviour {
+
+    void Awake() {
+        GvrViewer.Instance.VRModeEnabled = false;
+    }
+
+    void Update () {
+        if (GvrViewer.Instance.VRModeEnabled﻿ == true) {
+            GvrViewer.Instance.ToggleDeviceVRMode(false);
+        }
+
+        if (Camera.main.fieldOfView != 60) {
+            Camera.main.fieldOfView = 60;
+        }
+
+        if (GvrViewer.Instance.DistortionCorrection != GvrViewer.DistortionCorrectionMethod.None) {
+            GvrViewer.Instance.DistortionCorrection = GvrViewer.DistortionCorrectionMethod.None;
+        }
+
+        //GvrViewer.Distortion.Undistorted;
+
+
+        //this.transform.rotation = GvrController.Orientation;
+	}
+}
