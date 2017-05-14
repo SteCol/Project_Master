af9a92292dc3e50889ab01a9bf77416d665eae8a
Stef Colruyt <stef.col@hotmail.com>
2017-05-14 13:58:35 +0200

    Experimentatie.

--- Project_Master_Unity/Assets/Scripts/CustomGoogleVR.cs
+++ Project_Master_Unity/Assets/Scripts/CustomGoogleVR.cs
@@ -1,30 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Google;
+using UnityEngine.VR;
 
 public class CustomGoogleVR : MonoBehaviour {
 
     void Awake() {
-        GvrViewer.Instance.VRModeEnabled = false;
+        //GvrViewer.Instance.VRModeEnabled = false;
+        UnityEngine.VR.VRSettings.enabled = false;
+        this.GetComponent<Transform>().localRotation = UnityEngine.VR.InputTracking.GetLocalRotation(VRNode.CenterEye);
     }
 
     void Update () {
-        if (GvrViewer.Instance.VRModeEnabled﻿ == true) {
-            GvrViewer.Instance.ToggleDeviceVRMode(false);
-        }
+        //if (GvrViewer.Instance.VRModeEnabled﻿ == true) {
+        //    GvrViewer.Instance.ToggleDeviceVRMode(false);
+        //}
 
-        if (Camera.main.fieldOfView != 60) {
-            Camera.main.fieldOfView = 60;
-        }
+        //if (Camera.main.fieldOfView != 60) {
+        //    Camera.main.fieldOfView = 60;
+        //}
 
-        if (GvrViewer.Instance.DistortionCorrection != GvrViewer.DistortionCorrectionMethod.None) {
-            GvrViewer.Instance.DistortionCorrection = GvrViewer.DistortionCorrectionMethod.None;
-        }
+        //if (GvrViewer.Instance.DistortionCorrection != GvrViewer.DistortionCorrectionMethod.None) {
+        //    GvrViewer.Instance.DistortionCorrection = GvrViewer.DistortionCorrectionMethod.None;
+        //}
 
         //GvrViewer.Distortion.Undistorted;
-
+       
 
         //this.transform.rotation = GvrController.Orientation;
-	}
+    }
 }
